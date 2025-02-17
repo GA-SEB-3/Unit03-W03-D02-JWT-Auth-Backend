bdaee6bf325896c0e0783b1338b0a4920ecc5812
omarakamal <omaraakamal@gmail.com>
2025-02-17 19:13:35 +0300

    updated code

--- controllers/auth.routes.js
+++ controllers/auth.routes.js
@@ -1,7 +1,35 @@
+const User = require("../models/User")
+
 const router = require("express").Router()
+const bcrypt = require("bcrypt")
 
+router.post("/sign-up",async(req,res)=>{
+    try{
+        const foundUser = await User.findOne({username:req.body.username})
+        
+        if(foundUser){
+            return res.status(409).json({err:"username already taken"})
+        }
+        const createdUser = await User.create({
+            username:req.body.username,
+            hashedPassword: bcrypt.hashSync(req.body.password,12)
+        })
+        console.log(createdUser)
 
+        delete createdUser.hashedPassword
+        const {username,_id} = createdUser
+        res.json({username,_id})
 
+    }
+    catch(error){
+        res.status(500).json(error)
+    }
+
+})
+
+router.post("/login",(req,res)=>{
+
+})
 
 
 
