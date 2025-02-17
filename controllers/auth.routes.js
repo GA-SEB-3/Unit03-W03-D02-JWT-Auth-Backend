d263c7e866a7ea3bf59bc5607d226d263dd1a10b
omarakamal <omaraakamal@gmail.com>
2025-02-17 19:56:45 +0300

    updated code

--- controllers/auth.routes.js
+++ controllers/auth.routes.js
@@ -2,6 +2,7 @@ const User = require("../models/User")
 
 const router = require("express").Router()
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 router.post("/sign-up",async(req,res)=>{
     try{
@@ -16,9 +17,10 @@ router.post("/sign-up",async(req,res)=>{
         })
         console.log(createdUser)
 
-        delete createdUser.hashedPassword
-        const {username,_id} = createdUser
-        res.json({username,_id})
+
+        const convertedObject = createdUser.toObject()
+        delete convertedObject.hashedPassword
+        res.json(convertedObject)
 
     }
     catch(error){
@@ -27,8 +29,29 @@ router.post("/sign-up",async(req,res)=>{
 
 })
 
-router.post("/login",(req,res)=>{
+router.post("/login",async(req,res)=>{
+    // destructure the req.body
+    const {username,password} = req.body
+    try{
+        // 1. check if the user already signed up
+        const foundUser = await User.findOne({username})
 
+        // if there isnt a user this means that the user hasnt signed up yet
+        if(!foundUser){
+            return res.status(401).json({err:"username not signed up. Please sign up"})
+        }
+
+        // 2. check if the password given in the req.body matches the passowrd in the DB
+        const isPasswordMatch = bcrypt.compareSync(password,foundUser.hashedPassword)
+        if(!isPasswordMatch){
+            return res.status(401).json({err:"username or password incorrect"})
+        }
+        // 3. create the JWT token
+
+
+    }catch(error){
+        res.status(500).json(error)
+    }
 })
 
 
