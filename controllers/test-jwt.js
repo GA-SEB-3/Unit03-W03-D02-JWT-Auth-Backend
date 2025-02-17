fa6dc0043578928c38e533d1bad8e216413a5596
omarakamal <omaraakamal@gmail.com>
2025-02-17 18:53:58 +0300

    updated code

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -1,8 +1,21 @@
 const router = require("express").Router()
+const jwt = require("jsonwebtoken")
 
 router.get("/sign-token",(req,res)=>{
+    const user = {
+        _id:1,
+        name:"Loai"
+    }
 
-    res.json({message:"Route successful"})
+    // how to create a token
+    const token = jwt.sign({user},process.env.JWT_SECRET,{expiresIn:"1h"})
+    res.json({token:token})
+})
+
+
+router.post("/verify",(req,res)=>{
+    console.log(req.headers.authorization.split(" ")[1])
+    res.json({message:"Success"})
 })
 
 module.exports = router
